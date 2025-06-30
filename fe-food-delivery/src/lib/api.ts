f15c52a1731b1a5829d44569949e40edae4a8b3c
Anand <152850384+Anandshde@users.noreply.github.com>
2025-06-30 11:15:35 +0800

    chore: configure frontend to use remote API

--- fe-food-delivery/src/lib/api.ts
+++ fe-food-delivery/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://food-delivery-1-89kz.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
